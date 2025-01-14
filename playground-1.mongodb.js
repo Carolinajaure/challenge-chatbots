451d0f01b8275f5f910dabd117e164ed9c6bbaa6
unknown <94278@sistemas.frc.utn.edu.ar>
2025-01-14 17:51:01 -0300

    listo

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,22 +1,11 @@
-/* global use, db */
-// MongoDB Playground
-// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
-// Make sure you are connected to enable completions and to be able to run a playground.
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// The result of the last command run in a playground is shown on the results panel.
-// By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
-// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
-// Select the database to use.
-use('mongodbVSCodePlaygroundDB');
-// Insert a few documents into the sales collection.
-// Show existing collections in the database.
+
+use('mongodbVSCodePlaygroundDB');// seleccion de la base de datos a utilizar
+
 
 const collections = db.getCollectionNames();
 
-// Create the 'Entradas' collection if it doesn't exist
+
 if (collections.includes('Entradas')) {
     db.Entradas.deleteMany({});
     db.Entradas.insertMany([
@@ -30,7 +19,7 @@ if (collections.includes('Entradas')) {
 } else {
     console.log('Collection Entradas already exists.');
 }
-// Create the 'Gohan' collection if it doesn't exist
+
 if (!collections.includes('Gohan')) {
     db.createCollection('Gohan');
     db.Gohan.insertMany([
@@ -44,7 +33,7 @@ if (!collections.includes('Gohan')) {
 } else {
     console.log('Collection Gohan already exists.');
 }
-// Create the 'Rolls' collection if it doesn't exist
+
 if (!collections.includes('Rolls')) {
     db.createCollection('Rolls')
     db.Rolls.insertMany([
@@ -68,7 +57,7 @@ if (!collections.includes('Rolls')) {
 } else {
     console.log('Collection Rolls already exists.');
 }
-// Create the 'Combos' collection if it doesn't exist
+
 if (!collections.includes('Combos')) {
     db.createCollection('Combos');
     db.Combos.insertMany([
@@ -85,6 +74,6 @@ if (!collections.includes('Combos')) {
 } else {
     console.log('Collection Combos already exists.');
 }
-// Fetch the updated list of collections
+
 const updatedCollections = db.getCollectionNames();
 console.log('Collections:', updatedCollections);
